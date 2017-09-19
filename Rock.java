f2d32085616b1c71b9bd2867b6d9e7fb442ea89d
conhuang <30940887+conhuang@users.noreply.github.com>
2017-09-19 10:15:53 -0700

    Update Rock.java

--- Rock.java
+++ Rock.java
@@ -17,7 +17,7 @@ public class Rock
 
         Scanner scan = new Scanner(System.in);
 
-        System.out.println ("\fPlay Rock, Paper, Scissors!\n"+
+        System.out.print("\fPlay Rock, Paper, Scissors!\n"+
                         "Enter your play (\"R\" for Rock, \"P\" for paper, and \"S\" for scissors): ");
         personPlay = scan.nextLine();
         personPlay = personPlay.toUpperCase();//Make player's play uppercase for ease of comparison
@@ -32,28 +32,50 @@ public class Rock
         else
             computerPlay = "S";
 
-        System.out.println ("Computer play: " + computerPlay);//Print computer's play
+      
 
-        //See who won.  Use nested ifs instead of &&.
-        if (personPlay.equals(computerPlay))  
-            System.out.println("It's a tie!");
-        else if (personPlay.equals("R")){
-           if (computerPlay.equals("S"))
-                System.out.println("Rock crushes scissors.  You win!!");
-           else if (computerPlay.equals("P"))
-                System.out.println("Paper beats rock. You lose :(");
-        }
-         else if (personPlay.equals("S")){
-            if (computerPlay.equals("R"))
-                System.out.println("Rock crushes scissors. You lose :(");
-            else if (computerPlay.equals("P"))
-                System.out.println("Scissors cut up paper. You win!!");
-        }
-        else if (personPlay.equals("P")){
-            if (computerPlay.equals("R"))
-                System.out.println("Paper beats rock. You win!!");
-            else if (computerPlay.equals("S"))
-                System.out.println("Scissors cut up paper. You lose :(");
-        }
+        while (!personPlay.equals("Q")){
+             
+            if (personPlay.equals(computerPlay))  {
+                System.out.println ("Computer play: " + computerPlay);
+                System.out.println("It's a tie!");
+                System.out.print("\nEnter another play (q to quit): ");
+                personPlay = scan.nextLine();
+               personPlay = personPlay.toUpperCase();
+            } else if (personPlay.equals("R")){
+                System.out.println ("Computer play: " + computerPlay);
+                if (computerPlay.equals("S"))
+                    System.out.println("Rock crushes scissors.  You win!!");
+                else if (computerPlay.equals("P"))
+                    System.out.println("Paper beats rock. You lose :(");
+                System.out.print("\nEnter another play (q to quit): ");
+                personPlay = scan.nextLine();
+                personPlay = personPlay.toUpperCase();
+            }else if (personPlay.equals("S")){
+                System.out.println ("Computer play: " + computerPlay);
+                if (computerPlay.equals("R"))
+                    System.out.println("Rock crushes scissors. You lose :(");
+                else if (computerPlay.equals("P"))
+                    System.out.println("Scissors cut up paper. You win!!");
+                System.out.print("\nEnter another play (q to quit): ");
+                personPlay = scan.nextLine();
+                personPlay = personPlay.toUpperCase();
+           }else if (personPlay.equals("P")){
+               System.out.println ("Computer play: " + computerPlay);
+                if (computerPlay.equals("R"))
+                    System.out.println("Paper beats rock. You win!!");
+                else if (computerPlay.equals("S"))
+                    System.out.println("Scissors cut up paper. You lose :(");
+                System.out.print("\nEnter another play (q to quit): ");
+                    personPlay = scan.nextLine();
+                    personPlay = personPlay.toUpperCase();
+           }else{
+                System.out.print("Invalid input. Enter another play (q to quit): ");
+                personPlay = scan.nextLine();
+                personPlay = personPlay.toUpperCase();
+            }
+           
     }
+    System.out.println ("Thanks for playing!");
+}
 }
